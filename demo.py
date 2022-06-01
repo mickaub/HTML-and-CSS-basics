0ce781ed7ed2e0e85b2d7ff48bce2c0451671552
Michael Aubrey <mickaub3@gmail.com>
2022-06-01 11:24:30 +0800

    init

--- demo.py
+++ demo.py
@@ -1,1 +1,27 @@
-print("Hello")
+character_name = "John"
+character_age = "50"
+is_male = True
+phrase = "Python"
+print("\n")
+print(phrase.upper())
+print(" " + phrase[1])
+print("  " + phrase[2])
+print("   " + phrase[3])
+print("    " + phrase[4])
+print("     " + phrase[5])
+print("\n")
+print("This will be a triangle")
+print("\n")
+print("  /|")
+print(" / |")
+print("/__|")
+
+print("\n")
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ",")
+
+character_age = "in his 50's"
+print("but didn't like being " + character_age + ".")
+
+#https://www.youtube.com/watch?v=rfscVS0vtbw 48mins
